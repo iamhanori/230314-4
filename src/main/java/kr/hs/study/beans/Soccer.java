fd21ed4a77be1794462fe32d5fbff14139ac0cb5
Hanul Kim <s2122@e-mirim.hs.kr>
2023-03-14 15:58:25 +0900

    Soccer class 생성

---
+++ src/main/java/kr/hs/study/beans/Soccer.java
@@ -0,0 +1,9 @@
+package kr.hs.study.beans;
+
+public class Soccer implements Sports{
+    String name = "축구";
+    int player = 11;
+    public void rule(){
+        System.out.println("공을 차요!");
+    }
+}
