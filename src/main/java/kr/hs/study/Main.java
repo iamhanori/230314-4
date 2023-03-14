815413d8c3595822019cc675e707a37358c4be09
Hanul Kim <s2122@e-mirim.hs.kr>
2023-03-14 15:57:09 +0900

    baseball 객체 불러온 후 실행

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -10,5 +10,10 @@ public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
 
+        // 객체 불러오기
+        Sports baseball = ctx.getBean("baseball", BaseBall.class);
+        System.out.println(baseball.name);
+        System.out.println(baseball.player+"명");
+        baseball.rule();
     }
 }
